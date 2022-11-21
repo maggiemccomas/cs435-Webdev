b173070bf06cec694466c3eff3b9c012433a6424
Maggie McComas <112829059+maggiemccomas@users.noreply.github.com>
2022-11-21 15:22:51 -0500

    Maggie McComas HW6
    My changes to files index.js and App.js for HW 6 for Software Engineering.

--- src/App.js
+++ src/App.js
@@ -22,11 +22,23 @@ function App({ login }){
       return <pre>{JSON.stringify(error, null, 2)}</pre>;
     if(!data) return null;
 
-      return <div>
-        
+      return <center><div>
+        <h1>{data.name}</h1>
+        <p>{data.bio}</p>
+        <p>{data.location}</p>
+        <p>{"This is my profile picture!"}</p>
+        <img alt = {data.login} src = {data.avatar_url}/>
+        <p>{"This is another picture!"}</p>
+        <img alt = "A dog" src = "https://i.pinimg.com/originals/a6/3c/22/a63c22fc72b62b0624c3df5c0bff13ad.jpg"/>
+        <p>{"This is a button you can click!"}</p>
+        <button onClick={sayHello}>Default</button>
+        <p>{"Bye!"}</p>
       </div>
-
+      </center>
 }
 
+function sayHello(){
+  alert("You clicked me!")
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from "./App";
 
 ReactDOM.render(
+    <App login="maggiemccomas"/>,
     document.getElementById("root")
 );
 
